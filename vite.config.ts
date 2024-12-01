7d5a6f96037d1c9a10e4373ea87055ecf0322bff
Kjorgy <164044713+KJUDALL@users.noreply.github.com>
2024-12-01 12:35:50 -0700

    updates made to vite config to resolve errors deploying in render

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  envDir: './',
+  envDir: './',  // Ensure it looks in the correct directory for your .env files
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: parseInt(process.env.VITE_PORT || '3001'), // Access the VITE_PORT variable from .env
+  },
+  preview: {
+    host: '0.0.0.0', // Same for the preview server
+    port: parseInt(process.env.VITE_PORT || '3001'),
+  },
 });
